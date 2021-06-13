8d9843bf6d76a8ae31a789f6ca39c8cc4328fea3
thomas <thomas@thomass-MacBook-Pro.local>
2021-06-13 22:56:22 +0800

    更新

---
+++ Computer-Networking-A-Top-Down-Approach-Homework/Assignment-1/SocketServer.py
@@ -0,0 +1,40 @@
+#import socket module
+from socket import *
+serverSocket = socket(AF_INET, SOCK_STREAM)
+#Prepare a sever socket
+#Fill in start
+serverSocket.bind(('localhost',6999))
+serverSocket.listen(5)
+
+#Fill in end
+if(1):
+    #Establish the connection
+    print 'Ready to serve...'
+    connectionSocket, addr =  serverSocket.accept() #Fill in start  #Fill in end
+    try:
+        message =  connectionSocket.recv(1024) #Fill in start  #Fill in end
+        print(message)
+
+
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()#Fill in start  #Fill in end
+        #Send one HTTP header line into socket
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n")
+
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i])
+        connectionSocket.close()
+    except IOError:
+        print "meet an error"
+        #Send response message for file not found
+        #Fill in start
+        connectionSocket.send("HTTP/1.1 404 Not found\r\n\r\n")
+        #Fill in end
+
+        #Close client socket
+        #Fill in start
+        connectionSocket.close()
+        #Fill in end
+    serverSocket.close()
---
+++ Computer-Networking-A-Top-Down-Approach-Homework/Assignment-2/UDPPingerClient.py
@@ -0,0 +1,21 @@
+import socket
+from socket import *
+import datetime
+
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+for i in range(1, 10):
+    try:
+        begintime = datetime.datetime.now()
+        clientSocket.settimeout(1.0)
+        clientSocket.sendto('test', ('localhost', 12000))
+        message = clientSocket.recv(1024)
+        if message == 'TEST':
+            endtime = datetime.datetime.now()
+            rtt = endtime - begintime
+            print ('Ping ' + str(i) + ' ' + str(rtt))
+        else:
+            print 'ping message wrong'
+    except timeout:
+        print 'timeout'
+
